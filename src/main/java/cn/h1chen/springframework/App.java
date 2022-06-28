aeb29705ea9e95e9000942dbf2d1f3580b5041ef
hlchen <1204944512@qq.com>
2022-06-28 22:30:04 +0800

    feat: project init

---
+++ src/main/java/cn/h1chen/springframework/App.java
@@ -0,0 +1,10 @@
+package cn.h1chen.springframework;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
